c93c9eb6772c1d36e9c6d7ff02a9efa79fb62381
Vedant <vedantcoding31@gmail.com>
2022-10-15 23:53:26 +0530

    Update mergeSort.cpp

--- Sorting/mergeSort.cpp
+++ Sorting/mergeSort.cpp
@@ -4,12 +4,13 @@ using namespace std;
 
 void merge(int arr[], int low, int mid, int high) {
    
-    int n1 = mid - low + 1;
-    int n2 = high - mid;
+    int n1 = mid - low + 1; //to find array size of first sub-array
+    int n2 = high - mid; //to find array size of second sub array
 
     int * left_array = new int[n1]; 
     int * right_array = new int[n2]; //creating temp arrays for storing the original values of array
-
+	
+	//storing the values of original array in temp array
     for (int i = 0; i < n1; ++i) {
         left_array[i] = arr[low + i];
     }
@@ -19,10 +20,11 @@ void merge(int arr[], int low, int mid, int high) {
     }
 
     int i = 0, j = 0, k = low; //for tracking the array indices 
-
+	
+	//comparing elements of 1st sub array with 2nd sub array and then updating the original array
     while (i < n1 && j < n2) {
         if (left_array[i] < right_array[j]) {
-            arr[k] = left_array[i];
+            arr[k] = left_array[i]; 
             i++;
             k++;
         } else {
@@ -31,7 +33,8 @@ void merge(int arr[], int low, int mid, int high) {
             k++;
         }
     }
-
+	
+	//to update the remaining values to main array after one of the sub-array in exhausted
     while (i < n1) {
         arr[k] = left_array[i];
         i++;
