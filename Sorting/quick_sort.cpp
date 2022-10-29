babd80e666c5e76427661707510e4c21b5c369bf
Vedant <vedantcoding31@gmail.com>
2022-10-29 20:17:59 +0530

    Update quick_sort.cpp

--- Sorting/quick_sort.cpp
+++ Sorting/quick_sort.cpp
@@ -1,9 +1,13 @@
-//QUICK SORT
+// QUICK SORT
 #include <iostream>
 
 using namespace std;
 
-int partation(int arr[], int low, int high) {
+int partation(int arr[], int low, int high)
+{
+
+    int random = rand() % (high - low + 1) + low;
+    swap(arr[random], arr[high]); // swaping random to minimise the probablity of worst case of O(n^2)
 
     int pivot = arr[high];
     int i = low - 1; // i pointer to track the maximum element in array
@@ -13,37 +17,36 @@ int partation(int arr[], int low, int high) {
             i++;
             swap(arr[i], arr[j]);
         }
-
     }
 
     swap(arr[i + 1], arr[high]);
 
     return i + 1;
-
 }
 
-void quick_sort(int arr[], int low, int high) {
+void quick_sort(int arr[], int low, int high)
+{
     if (low < high) {
 
         int mid = partation(arr, low, high);
         quick_sort(arr, low, mid - 1);
         quick_sort(arr, mid + 1, high);
-
     }
 }
 
-void input(int arr[], int n) {
+void input(int arr[], int n)
+{
 
     cout << "Enter Elements: " << endl;
 
     for (int i = 0; i < n; ++i) {
 
         cin >> arr[i];
-
     }
 }
 
-void display(int arr[], int n) {
+void display(int arr[], int n)
+{
     cout << "Array is: " << endl;
 
     for (int i = 0; i < n; ++i) {
@@ -53,7 +56,8 @@ void display(int arr[], int n) {
     cout << endl;
 }
 
-int main() {
+int main()
+{
     int n;
     cout << "enter size of array: ";
     cin >> n;
@@ -67,5 +71,4 @@ int main() {
     printf("after sort: ");
 
     display(arr, n);
-
 }
