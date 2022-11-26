bf69c78ac17c178665f27037680639a70628e811
Vedant <vedantcoding31@gmail.com>
2022-11-26 23:07:08 +0530

    Create insertBeg.cpp

---
+++ Linked List/Circular single list/insertBeg.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
+
+Node* head = NULL;
+Node* tail = NULL;
+
+void insertBeg(int n)
+{
+    cout << "Enter data: ";
+    for (int i = 0; i < n; ++i) {
+        int data;
+        cin >> data;
+        Node* temp = new Node(data);
+
+        if (head == NULL) {
+            head = tail = temp;
+            tail->next = head;
+        } else {
+            temp->next = head;
+            head = temp;
+        }
+    }
+}
+
+void print(Node* list)
+{
+    cout << "List is: ";
+    while (list != tail) {
+        cout << list->data << " ";
+        list = list->next;
+        if (list == tail) {
+            cout << list->data;
+        }
+    }
+
+    cout << "\n";
+}
+int main()
+{
+
+    int n;
+    cout << "Enter total nodes to be inserted at beg: ";
+    cin >> n;
+    insertBeg(n);
+    print(head);
+}
