d3599f566b64cacc430ebd5b68945767712975ed
Vedant <vedantcoding31@gmail.com>
2022-12-20 14:46:11 +0530

    Create theMonkAndTheClassMarks.cpp

---
+++ STL/maps/theMonkAndTheClassMarks.cpp
@@ -0,0 +1,35 @@
+// the monk and the class marks
+//  given the marks of the students, print the marks in decreasing order and if two same name student exits then print them in the lexiographical order
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    map<int, multiset<string>> m;
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        string name;
+        int marks;
+        cin >> name >> marks;
+        m[marks].insert(name);
+    }
+
+    auto it = --m.end();
+
+    while (true) {
+        auto& students = it->second;
+        int marks = it->first;
+
+        for (auto student : students) {
+            cout << student << " " << marks << endl;
+        }
+
+        if (it == m.begin()) {
+            break;
+        }
+        it--;
+    }
+}
