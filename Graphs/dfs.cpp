596617900c079ce46adc173618c15cabe5c611b4
Vedant <91217411+VedantCode04@users.noreply.github.com>
2023-01-31 23:54:03 +0530

    Create dfs.cpp

---
+++ Graphs/dfs.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define loop(start, end) for (int i = start; i < end; ++i)
+#define loop1(start, end) for (int i = start; i <= end; ++i)
+#define loop2(start, end) for (int i = start; i >= end; --i)
+#define loop3(start, end) for (int i = start; i > end; --i)
+#define pb push_back
+#define vi vector<int>
+#define qi queue<int>
+#define si stack<int>
+#define ll long long
+
+int visit[9] = {0};
+vi vec;
+
+void adjList(vi list[], int edges)
+{
+    loop(0, edges)
+    {
+        int u, v;
+        cin >> u >> v;
+        list[u].pb(v);
+        list[v].pb(u);
+    }
+
+    // adjacency list o(edges*2) space complexit0
+}
+
+vi dfs(vi list[], int node, vi &vec)
+{
+	
+    visit[node] = 1;
+    vec.pb(node); //store the value in vector
+
+    for(auto a : list[node]){ //traversing every node connected to the list[node]
+    	if(visit[a] == 0){ //if the instance a is not visited, then perform dfs
+    		dfs(list, a, vec); //recussively traversing for 
+    	}
+    }
+
+    return vec;
+}
+
+int main()
+{
+    int nodes, edges;
+    cin >> nodes >> edges;
+
+    vi list[nodes];
+
+    adjList(list, edges);
+
+    cout << "graph is " << endl;
+
+    vi sol = dfs(list, 0, vec);
+
+    for (auto a : sol)
+        cout << a << " ";
+
+    return 0;
+}
